ea9e923699644df42ec470ad6ab58601fd0c63ee
Diego Ballesteros <yeahimdiego@gmail.com>
2020-07-04 11:25:06 -0500

    Continued adjustments to testing graphql queries for tweets

--- src/graphql/nodeTypes.js
+++ src/graphql/nodeTypes.js
@@ -1,21 +1,49 @@
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList } = require('graphql');
+const _ = require('lodash');
+
+var users = [
+    { username: 'User1', password: 'password', userId: '1' },
+    { username: 'User2', password: 'password', userId: '2' },
+    { username: 'User3', password: 'password', userId: '3' }
+];
+
+var tweets = [
+    { id: '1', userId: '1', content: 'First Tweet', date: "date" },
+    { id: '2', userId: '1', content: 'Second Tweet', date: "date" },
+    { id: '3', userId: '2', content: 'Third Tweet', date: "date" },
+    { id: '4', userId: '3', content: 'Fourth Tweet', date: "date" },
+    { id: '5', userId: '3', content: 'Fifth Tweet', date: "date" },
+    { id: '6', userId: '3', content: 'Sixth Tweet', date: "date" }
+];
 
 const tweetType = new GraphQLObjectType({
     name: 'Tweet',
-    fields: {
-        _id: { type: GraphQLID },
+    fields: () => ({
+        id: { type: GraphQLID },
         content: { type: GraphQLString },
-        date: { type: GraphQLString }
-    }
+        date: { type: GraphQLString },
+        user: {
+            type: userType,
+            resolve(parent, args) {
+                return _.find(users, { userId: parent.userId });
+            }
+        }
+    })
 });
 
 const userType = new GraphQLObjectType({
     name: 'User',
-    fields: {
-        _id: { type: GraphQLID },
+    fields: () => ({
+        userId: { type: GraphQLID },
         username: { type: GraphQLString },
-        password: { type: GraphQLString }
-    }
+        password: { type: GraphQLString },
+        tweets: {
+            type: new GraphQLList(tweetType),
+            resolve(parent, args) {
+                return _.filter(tweets, { userId: parent.userId});
+            }
+        }
+    })
 });
 
-module.exports = { tweetType, userType };
+module.exports = { userType, tweetType };
--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,16 +1,57 @@
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
-const { TweetsQuery } = require('./query/TweetsQuery');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = require('graphql');
+const { userType, tweetType } = require('./nodeTypes');
 const {
     CreateTweetMutation,
     UpdateTweetMutation,
     DeleteTweetMutation
 } = require('./mutation/TweetsMutation');
+const _ = require('lodash');
 
-const QueryType = new GraphQLObjectType({
-    name: 'Query',
-    fields: () => ({
-        tweets: TweetsQuery
-    })
+var users = [
+    { username: 'User1', password: 'password', userId: '1' },
+    { username: 'User2', password: 'password', userId: '2' },
+    { username: 'User3', password: 'password', userId: '3' }
+];
+
+var tweets = [
+    { id: '1', userId: '1', content: 'First Tweet', date: "date" },
+    { id: '2', userId: '1', content: 'Second Tweet', date: "date" },
+    { id: '3', userId: '2', content: 'Third Tweet', date: "date" },
+    { id: '4', userId: '3', content: 'Fourth Tweet', date: "date" },
+    { id: '5', userId: '3', content: 'Fifth Tweet', date: "date" },
+    { id: '6', userId: '3', content: 'Sixth Tweet', date: "date" }
+];
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        tweet: {
+            type: tweetType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(tweets, { id: args.id });
+            }
+        },
+        user: {
+            type: userType,
+            args: { username: { type: GraphQLString } },
+            resolve(parent, args) {
+                return _.find(users, { username: args.username });
+            }
+        },
+        tweets: {
+            type: new GraphQLList(tweetType),
+            resolve(parent, args) {
+                return tweets;
+            }
+        },
+        users: {
+            type: new GraphQLList(userType),
+            resolve(parent, args) {
+                return users;
+            }
+        }
+    }
 });
 
 const MutationType = new GraphQLObjectType({
@@ -22,6 +63,6 @@ const MutationType = new GraphQLObjectType({
     })
 });
 
-const schema = new GraphQLSchema({ query: QueryType, mutation: MutationType });
+const schema = new GraphQLSchema({ query: RootQuery });
 
 module.exports = schema;
